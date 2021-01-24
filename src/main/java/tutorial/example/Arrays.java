812590134126c027fb29996cc5ae3ae1cec9a82e
Tianli G <tianli4000@hotmail.com>
2021-01-24 13:17:56 -0500

    add examples

--- src/main/java/tutorial/example/Arrays.java
+++ src/main/java/tutorial/example/Arrays.java
@@ -1,2 +1,15 @@
-package tutorial.example;public class Arrays {
+package tutorial.example;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+
+
+        // declare and initialize
+        int[] array1 = {1, 2, 3};
+        int[] array2 = new int[]{1, 2, 3};
+    }
 }
--- src/main/java/tutorial/example/Variables.java
+++ src/main/java/tutorial/example/Variables.java
@@ -1,2 +1,11 @@
-package tutorial.example;public class Variables {
+package tutorial.example;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+    }
 }
