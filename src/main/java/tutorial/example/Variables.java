7fcac3533104a6ab3191b789c22f884fb650b578
i514097 <tianli.gao@sap.com>
2021-01-24 14:48:42 -0500

    add examples

--- src/main/java/tutorial/example/Variables.java
+++ src/main/java/tutorial/example/Variables.java
@@ -1,11 +1,14 @@
 package tutorial.example;
 
-public class Variables {
-    public static void main(String[] args) {
-        int myNum = 5;
-        float myFloatNum = 5.99f;
-        char myLetter = 'D';
-        boolean myBool = true;
-        String myText = "Hello";
-    }
+public class Variables
+{
+	public static void main(String[] args)
+	{
+		String studentName = "Tom Black";
+		int age = 22;
+		double gpa = 3.75;
+		boolean isFemale = true;
+
+		System.out.println(String.format("%s is %d years old. %s has a %s gpa.", studentName, age, (isFemale ? "She" : "He"), gpa));
+	}
 }
