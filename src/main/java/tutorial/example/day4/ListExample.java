aefaf1aa494884e87e7edfebbcccda01947a0bb7
i514097 <tianli.gao@sap.com>
2021-02-14 13:26:49 -0500

    add examples

--- src/main/java/tutorial/example/day4/ListExample.java
+++ src/main/java/tutorial/example/day4/ListExample.java
@@ -7,26 +7,30 @@ public class ListExample
 {
 	public static void main(String[] args) {
 		List<String> emails = new ArrayList<>();
-		emails.add("tianligao@hotmail.com");
-		emails.add("alexis@hotmail.com");
+		emails.add("whoami@hotmail.com");
+		emails.add("contact1@hotmail.com");
 
 		System.out.println("list size is " + emails.size());
 		System.out.println("first element is " + emails.get(0));
 		System.out.println("second element is " + emails.get(1));
 
-		emails.add(1, "chloe@hotmail.com");
+		System.out.println("----------------------");
+		
+		emails.add(1, "badguy@hotmail.com");
 
 		/**
-		 * 0->  tianligao@hotmail.com
-		 * 1->  choloe@hotmail.com
-		 * 2 -> alexis@hotmail.com
+		 * 0->  whoami@hotmail.com
+		 * 1->  badguy@hotmail.com
+		 * 2 -> contact1@hotmail.com
 		 */
 		System.out.println("list size is " + emails.size());
 		System.out.println("first element is " + emails.get(0));
 		System.out.println("second element is " + emails.get(1));
 		System.out.println("third element is " + emails.get(2));
 
-		List<String> contacts = List.of("Chloe", "Alexis", "Fenghua", "Annie", "Christina", "Jeremy");
+		System.out.println("----------------------");
+
+		List<String> contacts = List.of("Bob", "Mike", "Peter", "John", "Daniel", "Don");
 		contacts.forEach(c -> System.out.println(c));
 	}
 }
--- src/main/java/tutorial/example/day4/MapExample.java
+++ src/main/java/tutorial/example/day4/MapExample.java
@@ -8,23 +8,25 @@ public class MapExample
 	public static void main(String[] args)
 	{
 		Map<String, String> dictionary = new HashMap<>();
-		dictionary.put("science", "how to explain science");
-		dictionary.put("technology", "how to explain technology");
+		dictionary.put("Java", "Java is a OOP language");
+		dictionary.put("Python", "Python is great for bigdata");
 
-		System.out.println(dictionary.get("science"));
-
-		System.out.println("----------------------");
-
-		System.out.println(dictionary.get("technology"));
+		System.out.println(dictionary.get("Java"));
 
 		System.out.println("----------------------");
 
-		System.out.println(dictionary.get("math"));
+		System.out.println(dictionary.get("Python"));
+
+		System.out.println("----------------------");
+
+		System.out.println(dictionary.get("Go"));
+
+		System.out.println("----------------------");
 
 		System.out.println("map size is " + dictionary.size());
-		dictionary.entrySet().forEach(entry -> System.out.println("key is " + entry.getKey() + " value is " + entry.getValue()));
+		dictionary.entrySet().forEach(entry -> System.out.println("key is: " + entry.getKey() + ", value is: " + entry.getValue()));
 
-
+		System.out.println("----------------------");
 		Map<String, String> map = Map.of("key1", "value1", "key2", "value2");
 		map.entrySet().forEach(entry -> System.out.println(entry.getKey() + " -> " + entry.getValue()));
 
