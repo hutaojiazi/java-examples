f4391fe95db2187e7e5ca02d60c8247afcb4d53f
i514097 <tianli.gao@sap.com>
2021-05-04 12:01:02 -0400

    gradle

---
+++ src/test/java/tutorial/example/day13/Day13Test.java
@@ -0,0 +1,15 @@
+package tutorial.example.day13;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day13Test
+{
+	@Test
+	void test()
+	{
+		String str = "some text";
+		assertEquals("some text", str);
+	}
+}
